4e08c25b941885c106d486523e661a155941bd7c
stripathy <sujit.tripathy@icloud.com>
2016-09-28 17:09:14 -0700

    Initial commit

---
+++ src/main/java/com/hyperion/bookmarketplace/BookMarketPlaceAuthApplication.java
@@ -0,0 +1,12 @@
+package com.hyperion.bookmarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMarketPlaceAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMarketPlaceAuthApplication.class, args);
+	}
+}
